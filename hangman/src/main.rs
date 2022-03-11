479213d67de27838712505b359639aa9f8f78d0d
Jaish Singh <jaish.singh007@gmail.com>
2022-03-11 17:05:53 +0530

    .

--- hangman/src/main.rs
+++ hangman/src/main.rs
@@ -1,11 +1,25 @@
 use std::io;
+use rand::{thread_rng, Rng};
+#[allow(unused)]
 fn main() {
     let mut guesses =  Vec::new();
     let mut guesses_left = 6;
-    let word = String::from("ARBITRARY").into_bytes();
+    let mut words = Vec::new();
+    let temp_string = std::fs::read("words.txt").unwrap();
+    let mut last_pointer= 0 as usize;
+    for i in 0..temp_string.len()-1{
+        if temp_string[i] == 13{
+            &mut words.push(&temp_string[last_pointer..i]);
+            last_pointer = i+2;
+        }
+    }
+    let mut temp: u8 = thread_rng().gen();
+    temp = temp%words.len() as u8;
+    let word = words[temp as usize].to_ascii_uppercase();
     let mut display = format!("{}", "_".repeat(word.len()));
     println!("Word : {}", &display);
     while guesses_left > 0{
+        println!("-----------------------------------------------------");
         let mut guess = String::new();
         println!("Guess a letter");
         io::stdin().read_line(&mut guess).unwrap();
@@ -29,7 +43,6 @@ fn main() {
             println!("\x1b[92mYou Win!\x1b[0m");
             break;
         }
-        println!("-----------------------------------------------------");
         }
         if guesses_left==0{
             println!("\x1b[91mYOU LOSE\nThe word was {}\x1b[0m", String::from_utf8(word).unwrap());
